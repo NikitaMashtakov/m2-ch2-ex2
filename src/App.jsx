f2995dd5b877642c9ae6ce31dce2f74153fb0e5f
Nikita Mashtakov <n-mashtakov@yandex.ru>
2025-02-19 16:12:36 +0700

    finished exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import data from './data.json';
+const firstStep = Number(data[0].id);
+const lastStep = Number(data[data.length - 1].id);
+const listItemStyles = (id, activeStep) => {
+  return `${styles['steps-item']} ${id <= activeStep ? styles.done : null} ${
+    id == activeStep ? styles.active : null
+  }`;
+}; //вынес формирование стилей в отдельную функцию, чтобы был более компактный код в компоненте
 
 function App() {
-  const [activeStep, setActiveStep] = useState('');
-  const firstStep = '001';
-  const lastStep = '007';
-  const nextStep = (id) => {};
-  const previousStep = () => {};
-  const returnToStart = () => {};
+  const [activeStep, setActiveStep] = useState(0);
+
+  const nextStep = () => {
+    setActiveStep((prev) => prev + 1);
+  };
+
+  const previousStep = () => {
+    setActiveStep((prev) => prev - 1);
+  };
+
+  const returnToStart = () => {
+    setActiveStep(1);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.card}>
         <h1>Инструкция по готовке пельменей</h1>
         <div className={styles.steps}>
           <div className={styles['steps-content']}>
-            {data.find(({ id }) => id == activeStep)?.content || null}
+            {data.find(({ id }) => id == activeStep)?.content ||
+              'Нажмите начать для просмотра рецепта!'}
           </div>
           <ul className={styles['steps-list']}>
             {data.map(({ id, title }) => (
-              <li
-                className={
-                  styles['steps-item'] + ' ' + (id <= activeStep ? styles.done : null)
-                }
-                key={id}
-              >
+              <li className={listItemStyles(id, activeStep)} key={id}>
                 <button
                   className={styles['steps-item-button']}
                   onClick={() => {
-                    setActiveStep(id);
+                    setActiveStep(Number(id));
                   }}
                 >
                   {Number(id)}
@@ -38,11 +49,22 @@ function App() {
             ))}
           </ul>
           <div className={styles['buttons-container']}>
-            <button className={styles.button} disabled={activeStep === firstStep}>
+            <button
+              className={styles.button}
+              onClick={previousStep}
+              disabled={activeStep <= firstStep}
+            >
               Назад
             </button>
-            <button className={styles.button}>
-              {activeStep !== lastStep ? 'Далее' : 'Начать с начала'}
+            <button
+              className={styles.button}
+              onClick={activeStep !== lastStep ? nextStep : returnToStart}
+            >
+              {activeStep === 0
+                ? 'Начать'
+                : activeStep !== lastStep
+                ? 'Далее'
+                : 'Начать с начала'}
             </button>
           </div>
         </div>
